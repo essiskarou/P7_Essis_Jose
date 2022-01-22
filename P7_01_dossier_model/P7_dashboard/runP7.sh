0302b3d191074688245af32ab375e931b8000b26
Essis <essiskarou@yahoo.fr>
2022-01-22 15:21:07 +0100

    nouvel essai

--- P7_01_dossier_model/P7_dashboard/runP7.sh
+++
@@ -1,5 +0,0 @@
-#nohup python score.py  & 
-# sudo nohup docker run --publish 7878:5000 python-docker  &
-export FLASK_APP=P7
-flask run  --port=4567 --host=51.158.147.66
-#sudo -b docker run --publish 7878:5000 python-docker
